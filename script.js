e34b93d7ef9ec4c8ce89c93a4129d2297678bca0
Anshul <avenger720720@gmail.com>
2024-12-28 15:00:55 +0530

    completed

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ const headerContent = document.querySelector('.header-content')
 const closeIcon = document.querySelector('.close-icon')
 const goToTop = document.querySelector('.go-to-top')
 const main = document.querySelector('main-content')
+const nav = document.querySelector('nav')
 
-hamburgerIcon.addEventListener('click', () => {
+hamburgerIcon.addEventListener('click', (e) => {
+    e.stopPropagation()
     headerContent.classList.add('menu-open')
 })
 
+nav.addEventListener('click', (e) => {
+    e.stopPropagation()
+})
+
 closeIcon.addEventListener('click', () => {
+    headerContent.classList.remove('menu-open')
+})
+
+window.addEventListener('click', () => {
     headerContent.classList.remove('menu-open')
 })
